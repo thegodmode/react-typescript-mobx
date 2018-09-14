0346431443a462711f49a927509727e87daee448
Bozhidar Penchev <bozhidar.p@sbtech.com>
2018-09-14 12:57:17 +0300

    add mobx and a few fixes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,35 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+
+@observer
+export class HomeRoot extends React.Component<{ dataProvider: TestBussinesClass }, { dataProvider: TestBussinesClass }> {
+
+
+    constructor(props: any) {
+        super(props);
+
+        // setup data provider
+        this.state = {
+            dataProvider: this.props.dataProvider
+        }
+
+    }
+
+    render() {
+        return (<h1>Hello Bozho {this.state.dataProvider.message}</h1>)
+    }
+}
+
+export class TestBussinesClass {
+
+    @observable message = 1;
+}
+
 
 const ROOT = document.getElementById("root");
-ReactDOM.render(<h1>Hello</h1>, ROOT);
+var bussinessClass = new TestBussinesClass();
+(global as any)["SportsMultiViewBlock"] = bussinessClass;
+
+ReactDOM.render(<HomeRoot dataProvider={bussinessClass} />, ROOT);
